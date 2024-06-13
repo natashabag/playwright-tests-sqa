3c5af55ef6d3d36687dc74d443f21b2872eeb1dc
Natasha Bagramian <natalia.bagramian@gmail.com>
2024-06-12 21:33:55 -0700

    add tests for STU page

--- pages/stu-page.ts
+++ pages/stu-page.ts
@@ -3,22 +3,32 @@ import { Page, expect } from "@playwright/test";
 export class STUPage {
   constructor(private page: Page) {}
 
-  // Selectors
-  buttonSTU = this.page.locator("#menu-item-1183");
+  // Buttons:
   joinNowButtons = this.page
     .locator(".elementor-button.elementor-button-link.elementor-size-md")
     .filter({ hasText: "Join now" });
   joinNowTopButton = this.joinNowButtons.first();
-  joinNowButtonTwo = this.joinNowButtons.nth(2);
-  joinNowButtonThree = this.joinNowButtons.nth(3);
-
+  joinNowStuButton = this.joinNowButtons.nth(2);
+  joinNowStuLiteButton = this.joinNowButtons.nth(3);
+  joinSkoolButton = this.page
+    .locator(".elementor-button.elementor-button-link.elementor-size-md")
+    .filter({ hasText: "Join Skool" });
   joinStuButton = this.page
     .locator(".elementor-button.elementor-button-link.elementor-size-md")
     .filter({ hasText: "Join STU" });
+  closeButton = this.page.locator(
+    "//*[@id='elementor-lightbox-slideshow-single-img']/div/a"
+  );
 
+  // Heading:
   heading = this.page.getByRole("heading", {
     name: "Software Testing University",
   });
+
+  // Image:
+  image = this.page.locator(
+    ".elementor-element.elementor-element-2a1c644.elementor-widget.elementor-widget-image img"
+  );
 
   // URL
   stuURL = "https://softwarequalityassociates.com/stu/";
@@ -29,27 +39,53 @@ export class STUPage {
     await this.page.goto(this.stuURL);
   }
 
-  // Methods
-  async clickSTUButton() {
-    await this.buttonSTU.click();
-  }
-
+  // Button Methods:
   async clickJoinNowTopButton() {
     await this.joinNowTopButton.click();
   }
-  async clickJoinNowButtonTwo() {
-    await this.joinNowButtonTwo.scrollIntoViewIfNeeded();
-    await this.joinNowButtonTwo.waitFor({ state: "visible" });
-    await this.joinNowButtonTwo.click();
+  async clickJoinNowStuButton() {
+    await this.joinNowStuButton.scrollIntoViewIfNeeded();
+    await this.joinNowStuButton.waitFor({ state: "visible" });
+    await this.joinNowStuButton.click();
   }
-  async clickJoinNowButtonThree() {
-    await this.joinNowButtonThree.scrollIntoViewIfNeeded();
-    await this.joinNowButtonThree.waitFor({ state: "visible" });
-    await this.joinNowButtonThree.click();
+  async clickJoinNowStuLiteButton() {
+    await this.joinNowStuLiteButton.scrollIntoViewIfNeeded();
+    await this.joinNowStuLiteButton.waitFor({ state: "visible" });
+    await this.joinNowStuLiteButton.click();
+  }
+  async clickjoinStuButton() {
+    await this.joinStuButton.scrollIntoViewIfNeeded();
+    await this.joinStuButton.waitFor({ state: "visible" });
+    await this.joinStuButton.click();
   }
 
+  async clickjoinSkoolButton() {
+    await this.joinSkoolButton.scrollIntoViewIfNeeded();
+    await this.joinSkoolButton.waitFor({ state: "visible" });
+    await this.joinSkoolButton.click();
+  }
+
+  // Click Image:
+  async clickImage() {
+    await this.image.scrollIntoViewIfNeeded();
+    await this.image.waitFor({ state: "visible" });
+    await this.image.click();
+  }
+
+  async closeImage() {
+    await this.closeButton.click();
+  }
+  // Verification Methods:
   async verifyJoinNowButtons() {
     await expect(this.joinNowButtons).toHaveCount(4);
+  }
+
+  async verifyCloseButtonDisplayed() {
+    await expect(this.closeButton).toBeVisible();
+  }
+
+  async verifyCloseButtonNotDisplayed() {
+    await expect(this.closeButton).toBeHidden();
   }
 
   async verifySTUPage() {
